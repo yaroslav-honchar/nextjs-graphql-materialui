cd76384c7d011ff822b175f979a51fd7a31c454f
Yaroslav Honchar <yaroslav.honchar.1@gmail.com>
2024-09-25 20:55:33 +0300

    fix: clenup home page

--- src/_pages/Home/HomePage.ui.tsx
+++ src/_pages/Home/HomePage.ui.tsx
@@ -1,44 +1,8 @@
-import { gql } from "graphql-tag"
-import { getClient } from "@/_app/graphql/gql-register-client"
 import { unstable_setRequestLocale } from "next-intl/server"
-import Image from "next/image"
 import type { IHomePageProps } from "./HomePage.props"
-
-const query = gql`
-  {
-    movies {
-      total_pages
-      total_results
-      page
-      results {
-        title
-        release_date
-        poster_path
-        id
-      }
-    }
-  }
-`
 
 export async function HomePage({ params: { locale } }: IHomePageProps) {
   unstable_setRequestLocale(locale)
 
-  const { data } = await getClient().query({ query })
-
-  return (
-    <ul>
-      {data.movies.results.map((movie) => (
-        <li key={movie.id}>
-          <h2>{movie.title}</h2>
-          <p>{movie.release_date}</p>
-          <Image
-            width={150}
-            height={225}
-            alt={movie.title}
-            src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-          />
-        </li>
-      ))}
-    </ul>
-  )
+  return <></>
 }
